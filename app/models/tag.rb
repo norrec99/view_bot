19f5fa7b12ce7aa5ec26c830cfa4ed0a0414fe46
Cagatay Soyer <cagataysoyer@gmail.com>
2020-10-26 18:01:29 +0300

    created controllers

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,5 +1,6 @@
 class Tag < ApplicationRecord
   has_one :review_tag
+  # should we use "through" or not ?
 
   validates :name, presence: true, inclusion: { in: ["Frontend", "Backend", "Design", "Good", "Bad"] }
 end
