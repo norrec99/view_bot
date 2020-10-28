25d830a6b57333d4e69963987f35d17420354fcb
ozan isgor <ozanisgor11@gmail.com>
2020-10-28 13:00:45 +0300

    index method added to applications controller

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -2,4 +2,13 @@ class ApplicationsController < ApplicationController
   def show
     @application = Application.find(params[:id])
   end
+
+  def index
+    if params[:query].present?
+      sql_query = "applications.name @@ :query"
+      @applications = Application.where(sql_query, query: "%#{params[:query]}%")
+    else
+      @applications = Application.all
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   
-  resources :applications, only: [ :show ] 
+  resources :applications, only: [ :show, :index ] 
   resources :reviews, only: [ :edit, :update, :show ]
   get "/search", to: "pages#search"
 end
