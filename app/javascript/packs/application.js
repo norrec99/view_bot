d489c8fff95ca1338f60432dec3b7e74f5f4e9be
leena khalili <engleena@yahoo.com>
2020-10-29 18:50:53 +0300

    API and review path update

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,38 +1,75 @@
 // This file is automatically compiled by Webpack, along with any other files
+
 // present in this directory. You're encouraged to place your actual application logic in
+
 // a relevant structure within app/javascript and only use these pack files to reference
+
 // that code so it'll be compiled.
 
+
+
 require("@rails/ujs").start()
+
 require("turbolinks").start()
+
 require("@rails/activestorage").start()
+
 require("channels")
 
 
+
+
+
 // Uncomment to copy all static images under ../images to the output folder and reference
+
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
+
 // or the `imagePath` JavaScript helper below.
+
 //
+
 // const images = require.context('../images', true)
+
 // const imagePath = (name) => images(name, true)
 
 
+
+
+
 // ----------------------------------------------------
+
 // Note(lewagon): ABOVE IS RAILS DEFAULT CONFIGURATION
+
 // WRITE YOUR OWN JS STARTING FROM HERE 👇
+
 // ----------------------------------------------------
+
+
 
 // External imports
+
 import "bootstrap";
+
 // Internal imports, e.g:
+
 import { multipleSelect } from '../components/select';
 
+import { initSearchApplication } from '../components/init_search_application';
+
+import { initPostApplication } from '../utils/fetch_with_token';
 
 
 
 document.addEventListener('turbolinks:load', () => {
-  // Call your functions here, e.g:
-  // initSelect2();
-  multipleSelect();
-});
 
+  // Call your functions here, e.g:
+
+  // initSelect2();
+
+  multipleSelect();
+
+  initSearchApplication();
+
+  initPostApplication();
+
+});
---
+++ app/javascript/utils/fetch_with_token.js
@@ -0,0 +1,73 @@
+import { csrfToken } from "@rails/ujs";
+
+
+
+const fetchWithToken = (url, options) => {
+
+  options.headers = {
+
+    "X-CSRF-Token": csrfToken(),
+
+    ...options.headers
+
+  };
+
+  options.credentials = "same-origin";
+
+
+
+  return fetch(url, options);
+
+}
+
+
+
+const postApplication = (name) => {
+
+  fetchWithToken("/applications", {
+
+    method: "POST",
+
+    headers: {
+
+      "Accept": "application/json",
+
+      "Content-Type": "application/json"
+
+    },
+
+    body: JSON.stringify({ application: { name: name }}) 
+  })
+    .then(response => response.json()) 
+    .then((data) => {
+      window.location.href = '/applications';
+    })
+   
+
+  
+
+}
+
+
+
+const initPostApplication = () => {
+
+  const btns = document.querySelectorAll('.js-application');
+
+  btns.forEach((btn) => {
+
+    btn.addEventListener('click', (event) => {
+
+      event.preventDefault();
+
+      postApplication(btn.dataset.title);
+
+    })
+
+  } )
+
+}
+
+
+
+export { initPostApplication }
