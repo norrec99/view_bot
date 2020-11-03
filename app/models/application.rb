8687a7df942e85e3130de62d8f2f8b14b0ecfa09
Cagatay Soyer <cagataysoyer@gmail.com>
2020-11-03 11:02:25 +0300

    application uniqueness added

--- app/models/application.rb
+++ app/models/application.rb
@@ -1,6 +1,6 @@
 class Application < ApplicationRecord
   has_many :reviews
   has_many :users
-  # validates :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
   has_one_attached :photo
 end
