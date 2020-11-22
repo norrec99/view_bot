35d6fbb2e87f385351554c29b068f3d4cf40b2f2
Cagatay Soyer <cagataysoyer@gmail.com>
2020-11-22 21:24:20 +0300

    host changed for reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -120,5 +120,5 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
   }
-  config.action_mailer.default_url_options = { :host => 'viewbotshop.xyz/' }
+  config.action_mailer.default_url_options = { :host => 'viewbotshop.xyz' }
 end
