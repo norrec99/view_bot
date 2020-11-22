dcc00a67bf79c7d7e69f81b404da58cccf9eebc7
Cagatay Soyer <cagataysoyer@gmail.com>
2020-11-22 21:48:26 +0300

    host changed for reset password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,11 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "view_bot_production"
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -120,8 +120,5 @@ Rails.application.configure do
     user_name: ENV["GMAIL_USERNAME"],
     password:ENV["GMAIL_PASSWORD"]
   }
-  config.action_mailer.default_url_options = { 
-  host: 'www.viewbotshop.xyz', 
-  protocol: 'https'
-  }
+  config.action_mailer.default_url_options = { :host => 'viewbotshop.xyz' }
 end
