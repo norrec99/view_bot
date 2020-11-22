9e4634d18a37fcc1aaa52085e141315b9bc993a2
Cagatay Soyer <cagataysoyer@gmail.com>
2020-11-22 23:15:56 +0300

    fixed indentation

--- app/controllers/review_tags_controller.rb
+++ app/controllers/review_tags_controller.rb
@@ -1,15 +1,10 @@
 class ReviewTagsController < ApplicationController
   def create
-    
     @review = Review.find(params[:review_id])
     @tags = Tag.where(id: params[:review_tag][:tag])
-
     @tags.each do |tag|
-      review_tag = ReviewTag.create!(review: @review, tag: tag)
+      ReviewTag.create!(review: @review, tag: tag)
     end
-
     redirect_to edit_review_path(@review), notice: 'Your tags were successfully added.'
-
   end
-
 end
